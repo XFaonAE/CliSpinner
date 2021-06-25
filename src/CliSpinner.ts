17cbe72eb9c194c84240e1d170a2d4638db22375
Rayyan <85759101+XfaonAE@users.noreply.github.com>
2021-06-24 20:12:18 -0400

    Changed visibility method

--- src/CliSpinner.ts
+++ src/CliSpinner.ts
@@ -1,162 +1,163 @@
 import Chalk from "chalk";
 
-export namespace axeri {
-    export class CliSpinner {
-        /**
-         * List of all the frames the spinner animation can run through in order [ 0 -> max ]
-         * @var { string[] }
-         */
-        public static spinnerFrames: string[] = [
-            Chalk.hex("#50ffab")("⠋"), 
-            Chalk.hex("#50ffab")("⠙"), 
-            Chalk.hex("#50ffab")("⠹"), 
-            Chalk.hex("#50ffab")("⠸"), 
-            Chalk.hex("#50ffab")("⠼"), 
-            Chalk.hex("#50ffab")("⠴"), 
-            Chalk.hex("#50ffab")("⠦"), 
-            Chalk.hex("#50ffab")("⠧"), 
-            Chalk.hex("#50ffab")("⠇"), 
-            Chalk.hex("#50ffab")("⠏")
-        ];
+class CliSpinner {
+    /**
+     * List of all the frames the spinner animation can run through in order [ 0 -> max ]
+     * @var { string[] }
+     */
+    public spinnerFrames: string[] = [
+        Chalk.hex("#50ffab")("⠋"), 
+        Chalk.hex("#50ffab")("⠙"), 
+        Chalk.hex("#50ffab")("⠹"), 
+        Chalk.hex("#50ffab")("⠸"), 
+        Chalk.hex("#50ffab")("⠼"), 
+        Chalk.hex("#50ffab")("⠴"), 
+        Chalk.hex("#50ffab")("⠦"), 
+        Chalk.hex("#50ffab")("⠧"), 
+        Chalk.hex("#50ffab")("⠇"), 
+        Chalk.hex("#50ffab")("⠏")
+    ];
 
-        /**
-         * Identifies what frame the spinner is currently on
-         * @var { number }
-         */
-        public static spinnerCurrentFrameId: number = 1;
+    /**
+     * Identifies what frame the spinner is currently on
+     * @var { number }
+     */
+    public spinnerCurrentFrameId: number = 1;
 
-        /**
-         * Delay between each frame of the spinner in milliseconds
-         * @var { number }
-         */
-        public static spinnerFrameDelay: number = 50;
+    /**
+     * Delay between each frame of the spinner in milliseconds
+     * @var { number }
+     */
+    public spinnerFrameDelay: number = 50;
 
-        /**
-         * Is the spinner frame loop running
-         * @var { boolean }
-         */
-        public static spinnerFrameLoopStarted: boolean = false;
+    /**
+     * Is the spinner frame loop running
+     * @var { boolean }
+     */
+    public spinnerFrameLoopStarted: boolean = false;
 
-        /**
-         * Should the spinner render its frames
-         * @var { boolean }
-         */
-        public static spinnerLoopRendering: boolean = false;
+    /**
+     * Should the spinner render its frames
+     * @var { boolean }
+     */
+    public spinnerLoopRendering: boolean = false;
 
-        /**
-         * Spinner message to display
-         * @var { string }
-         */
-        public static spinnerMessage: string = "";
+    /**
+     * Spinner message to display
+     * @var { string }
+     */
+    public spinnerMessage: string = "";
 
-        /**
-         * Last rendered output from this class
-         */
-        public static lastSentOutput: string = "";
+    /**
+     * Last rendered output from this class
+     */
+    public lastSentOutput: string = "";
 
-        /**
-         * Start the loading animation with some text followed after the spinner
-         * @param { string } message Message to print with loading animation
-         */
-        public static write(message: string) {
-            // Start the frame loop if it hasn't been started yet
-            if (!this.spinnerFrameLoopStarted) {
-                this.spinnerFrameLoopStarted = true;
-                this.runSpinnerLoop();
-            }
-
-            // Set spinner message
-            this.spinnerMessage = message;
-
-            // Allow spinner to start rendering
-            this.spinnerLoopRendering = true;
+    /**
+     * Start the loading animation with some text followed after the spinner
+     * @param { string } message Message to print with loading animation
+     */
+    public write(message: string) {
+        // Start the frame loop if it hasn't been started yet
+        if (!this.spinnerFrameLoopStarted) {
+            this.spinnerFrameLoopStarted = true;
+            this.runSpinnerLoop();
         }
 
-        /**
-         * Stop spinner animation
-         */
-        public static stop(icon: string, message: string|null = null) {
-            // Stop rendering
-            this.spinnerLoopRendering = false;
-            
-            // Clear line
-            process.stdout.write("\r" + this.getClearLine());
+        // Set spinner message
+        this.spinnerMessage = message;
 
-            // Set closing message
-            var closingMessage = "";
-            if (message) {
-                closingMessage = message;
-            } else {
-                closingMessage = this.spinnerMessage; 
-            }
+        // Allow spinner to start rendering
+        this.spinnerLoopRendering = true;
+    }
 
-            // Write closing message
-            process.stdout.write("\r" + icon + " " + closingMessage);
-            console.log("");
+    /**
+     * Stop spinner animation
+     */
+    public stop(icon: string, message: string|null = null) {
+        // Stop rendering
+        this.spinnerLoopRendering = false;
+        
+        // Clear line
+        process.stdout.write("\r" + this.getClearLine());
+
+        // Set closing message
+        var closingMessage = "";
+        if (message) {
+            closingMessage = message;
+        } else {
+            closingMessage = this.spinnerMessage; 
         }
 
-        /**
-         * Run the spinner frame loop
-         */
-        public static runSpinnerLoop() {
-            const loop = () => {
-                setTimeout(() => {
-                    // Check if the loop should render anything
-                    if (this.spinnerLoopRendering) {
-                        this.lastSentOutput = "\r" + this.getNextFrame() + " " + this.spinnerMessage;
-                        process.stdout.write(this.lastSentOutput);
-                    }
+        // Write closing message
+        process.stdout.write("\r" + icon + " " + closingMessage);
+        console.log("");
+    }
 
-                    // Run the loop's next iteration
-                    if (this.spinnerFrameLoopStarted) {
-                        loop();
-                    }
-                }, this.spinnerFrameDelay);
-            }
-            
-            // Start loop
-            loop();
+    /**
+     * Run the spinner frame loop
+     */
+    public runSpinnerLoop() {
+        const loop = () => {
+            setTimeout(() => {
+                // Check if the loop should render anything
+                if (this.spinnerLoopRendering) {
+                    this.lastSentOutput = "\r" + this.getNextFrame() + " " + this.spinnerMessage;
+                    process.stdout.write(this.lastSentOutput);
+                }
+
+                // Run the loop's next iteration
+                if (this.spinnerFrameLoopStarted) {
+                    loop();
+                }
+            }, this.spinnerFrameDelay);
+        }
+        
+        // Start loop
+        loop();
+    }
+
+    /**
+     * Fully stop spinner frame loops
+     */
+    public fullStop() {
+        this.spinnerFrameLoopStarted = false;
+    }
+
+    /**
+     * Returns a clear line which is the same size as the last sent output
+     * @returns { string }
+     */
+    public getClearLine() {
+        // Get character count in last output
+        const characters = this.lastSentOutput.length;
+
+        // Build the clear line
+        var clearLine = "";
+        while (clearLine.length < characters) {
+            clearLine += " ";
         }
 
-        /**
-         * Fully stop spinner frame loops
-         */
-        public static fullStop() {
-            this.spinnerFrameLoopStarted = false;
-        }
+        // Return the clear line
+        return clearLine;
+    }
 
-        /**
-         * Returns a clear line which is the same size as the last sent output
-         * @returns { string }
-         */
-        public static getClearLine() {
-            // Get character count in last output
-            const characters = this.lastSentOutput.length;
+    /**
+     * Get the next frame raw
+     * @returns { string } Raw frame
+     */
+    public getNextFrame() {
+        // Increment frames ID by 1 if not at max
+        if (this.spinnerCurrentFrameId == this.spinnerFrames.length) {
+            this.spinnerCurrentFrameId = 1;
+        } else {
+            this.spinnerCurrentFrameId++;
+        } 
 
-            // Build the clear line
-            var clearLine = "";
-            while (clearLine.length < characters) {
-                clearLine += " ";
-            }
-
-            // Return the clear line
-            return clearLine;
-        }
-
-        /**
-         * Get the next frame raw
-         * @returns { string } Raw frame
-         */
-        public static getNextFrame() {
-            // Increment frames ID by 1 if not at max
-            if (this.spinnerCurrentFrameId == this.spinnerFrames.length) {
-                this.spinnerCurrentFrameId = 1;
-            } else {
-                this.spinnerCurrentFrameId++;
-            } 
-
-            // Return the next frame as a string
-            return this.spinnerFrames[this.spinnerCurrentFrameId - 1];
-        }
+        // Return the next frame as a string
+        return this.spinnerFrames[this.spinnerCurrentFrameId - 1];
     }
 }
+
+const cliSpinner = new CliSpinner();
+export default cliSpinner;
